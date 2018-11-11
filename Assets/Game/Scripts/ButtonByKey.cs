c933255cbb11fa8b87af3371800e1d7ab1707d50
roverem <martinr@qb9.net>
2018-11-11 19:50:08 -0300

    rompi todo y lo volvi a montar

---
+++ Assets/Game/Scripts/ButtonByKey.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonByKey : MonoBehaviour {
+
+	public KeyCode _Key;
+	private Button _button;
+
+	void Awake()
+	{
+		_button = GetComponent<Button>();
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown(_Key))
+		{
+			FadeToColor(_button.colors.pressedColor);
+			_button.onClick.Invoke();
+		}
+		else if (Input.GetKeyUp(_Key))
+		{
+			FadeToColor(_button.colors.normalColor);
+		}
+	}
+
+	void FadeToColor(Color color) 
+	{
+		Graphic graphic = GetComponent<Graphic>();
+		graphic.CrossFadeColor(color, _button.colors.fadeDuration, true, true);
+	}
+}
